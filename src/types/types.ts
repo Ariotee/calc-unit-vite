a59db29b310d80ca2dca63a2c51c48cb7c3f61cc
SoloTufi <mat_990011@mail.ru>
2024-04-13 15:36:08 +0700

    types
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export interface IPropsFrom {
+	setCurrentUnitFrom: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export interface IPropsTo {
+	setCurrentUnitTo: React.Dispatch<React.SetStateAction<string>>;
+}
