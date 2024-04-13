44901341c2c99a05403aeb7bb98dab54dae83751
SoloTufi <mat_990011@mail.ru>
2024-04-13 15:36:34 +0700

    data
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

---
+++ src/data/units.ts
@@ -0,0 +1,4 @@
+export const length = ['millimeter', 'centimeter', 'decimeter', 'meter', 'kilometer'];
+export const weight = ['milligram', 'gram', 'kilogram', 'hundredweight', 'ton'];
+export const time = ['millisecond', 'second', 'minute', 'hour'];
+export const unitsInformation = ['bit', 'byte', 'Kbyte', 'Mbyte', 'Gbyte', 'Tbyte'];
